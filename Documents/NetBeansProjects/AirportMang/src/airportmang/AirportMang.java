4dab90328964036984b18bac094066dd3daef125
dweeppandya <dweep.pandya@gmail.com>
2017-08-16 22:21:34 +0530

    second

--- Documents/NetBeansProjects/AirportMang/src/airportmang/AirportMang.java
+++ Documents/NetBeansProjects/AirportMang/src/airportmang/AirportMang.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package airportmang;
+import java.io.IOException;
 import java.sql.*;
 /**
  *
@@ -14,8 +15,27 @@ public class AirportMang {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(log_in.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(log_in.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(log_in.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(log_in.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        log_in login = new log_in();
+        login.setVisible(true);
     }
     
 }
